16c4075d6c15dc8ae6404d10e24cce8cbd3add63
qasunVJ <wijeratna.e.kasun@gmail.com>
2017-01-17 15:16:55 +0530

    Image Uploader

--- product/product.controller.js
+++ product/product.controller.js
@@ -1308,7 +1308,7 @@
 
         if(files.length > 0) {
           $scope.productImgFileName = files[0].name;
-          $scope.productImgFileType = files[0].type;
+          $scope.productImgFileType = files[0].type.split('/')[1];
         }
       });
     }
